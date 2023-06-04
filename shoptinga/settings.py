b25be1e6898dec3d0867ae9bfbac96687030a64b
tee-o28 <tee.o2809@gmail.com>
2023-06-04 18:57:48 +0100

    Update settings.py

--- shoptinga/settings.py
+++ shoptinga/settings.py
@@ -88,26 +88,33 @@ WSGI_APPLICATION = 'shoptinga.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
+# DATABASES = {
+
+#     # 'default': {
+
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+
+#     #     'NAME': BASE_DIR / 'db.sqlite3',
+
+#     # }
+
+#     # 'default': {
+#     #     'ENGINE' : 'django.db.backends.postgresql',
+#     #     'URL' : env('db_URL'),
+#     #     'NAME' : 'neondb',
+#     #     'USER' : 'kiddo4',
+#     #     'PASSWORD' : env('db_PASSWORD'),
+#     #     'HOST' : env('db_HOST'),
+#     #     'PORT' : '5432',
+#     # }
+
+# }
+
 DATABASES = {
-
-    # 'default': {
-
-    #     'ENGINE': 'django.db.backends.sqlite3',
-
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-
-    # }
-
     'default': {
-        'ENGINE' : 'django.db.backends.postgresql',
-        'URL' : env('db_URL'),
-        'NAME' : 'neondb',
-        'USER' : 'kiddo4',
-        'PASSWORD' : env('db_PASSWORD'),
-        'HOST' : env('db_HOST'),
-        'PORT' : '5432',
-    }
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    } 
 }
 
 # Password validation
